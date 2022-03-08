22cdb7aad5563e6a9f3654f3bdbc22cf351036c7
Даниил Кладько <daniilkladko@MacBook-Air-Daniil-2.local>
2022-03-08 02:25:23 +0100

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for z in range(1000):
+    
+    p.stepSimulation()
+    time.sleep( 1/60 )
+    print(z)
+
 
 p.disconnect() 
